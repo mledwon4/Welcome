c09d67f3a447d73f3475537a142a68a589259429
Matt Ledwon <matthew.j.ledwon.-ND@disney.com>
2015-05-22 15:42:45 -0500

    Welcome app initial commit

---
+++ Welcome/Welcome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Welcome
+//
+//  Created by Matt Ledwon on 5/22/15.
+//  Copyright (c) 2015 Matt's Demos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Welcome/Welcome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Welcome
+//
+//  Created by Matt Ledwon on 5/22/15.
+//  Copyright (c) 2015 Matt's Demos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
