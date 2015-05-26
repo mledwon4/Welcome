01fa412bcd6ea56605801ade5f875dfec2af81ca
Matt Ledwon <matthew.j.ledwon.-ND@disney.com>
2015-05-26 11:25:20 -0400

    Added unit tests

--- Welcome/WelcomeTests/WelcomeTests.m
+++ Welcome/WelcomeTests/WelcomeTests.m
@@ -15,26 +15,28 @@
 
 @implementation WelcomeTests
 
+int apple = 100;
+int banana = 200;
+int android = 50;
+
 - (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each
+  // test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+  // Put teardown code here. This method is called after the invocation of each
+  // test method in the class.
+  [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testAppleBetterThanAndroid {
+  XCTAssertGreaterThan(apple, android);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testAppleEqualsBanana {
+  XCTAssertEqual(apple, banana);
 }
 
 @end
